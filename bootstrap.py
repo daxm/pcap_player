58fa62752eb00bb488d811a9e78ca13c50c4fd88
Dax Mickelson <dmickels@cisco.com>
2021-06-30 10:16:53 -0600

    Add requirements and gitignore

--- bootstrap.py
+++ bootstrap.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
+"""Generate traffic by playing a PCAP file onto the network."""
 
 from scapy.all import *
 import sys
 
-for sniffed_packet in sniff(offline=sys.argv[1]):
-    # print(sniffed_packet)
-    sendp(sniffed_packet)
+verbose = False
 
+
+def main():
+    for sniffed_packet in sniff(offline=sys.argv[1]):
+        if verbose:
+            print(sniffed_packet)
+        sendp(sniffed_packet)
+
+
+if __name__ == "__main__":
+    main()
